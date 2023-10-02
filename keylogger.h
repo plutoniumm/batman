29313162b71ddbdf998bc78c32375164df0211cf
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-10-02 08:34:36 +0530

    format

--- keylogger.h
+++ keylogger.h
@@ -1,18 +1,18 @@
 #ifndef __KEYLOGGER_H__
 #define __KEYLOGGER_H__
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <time.h>
-#include <string.h>
 #include <ApplicationServices/ApplicationServices.h>
 #include <Carbon/Carbon.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 // https://developer.apple.com/library/mac/documentation/Carbon/Reference/QuartzEventServicesRef/Reference/reference.html
 
 FILE *logfile = NULL;
 const char *logfileLocation = "/var/log/keystroke.log";
 
-CGEventRef CGEventCallback(CGEventTapProxy, CGEventType, CGEventRef, void*);
+CGEventRef CGEventCallback(CGEventTapProxy, CGEventType, CGEventRef, void *);
 const char *convertKeyCode(int, bool, bool);
 
 #endif
